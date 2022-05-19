2b1d585823cfb3522738541ba9bd90cd9c8cc866
MarieSerroyen <marie.serroyen@gmail.com>
2022-05-19 12:07:25 +0200

    feat: get user coins works with api

--- classes/App.js
+++ classes/App.js
@@ -1,3 +1,26 @@
 export default class App {
-    
+    constructor() {
+        this.getCoins();
+    }
+
+    getCoins() {
+
+        fetch('https://currency-backend-mms.herokuapp.com/api/v1/getCoins', {
+            method: "get",
+            headers: {
+                'Content-Type': 'application/json',
+                "Authorization": "Bearer " + localStorage.getItem('token')
+            }
+        }).then(response => {
+            return response.json();
+        }).then(json => {
+            if(json.status === "success") {
+                console.log("👍👌👌");
+                
+            } if(json.status === "error") {
+                console.log(json.message);
+                
+            }
+        })
+    }
 }
