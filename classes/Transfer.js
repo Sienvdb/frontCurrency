293008246eccd21a9042bccdaf76cb0cdf668e80
MarieSerroyen <marie.serroyen@gmail.com>
2022-05-20 10:25:06 +0200

    feat: show date on transaction index

--- classes/Transfer.js
+++ classes/Transfer.js
@@ -22,15 +22,18 @@ export default class Transfer {
                     result = [...result, ...Object.values(item)];
                     console.log(result[5]);
 
-                    
                     let name = `<h4> 
-                            <span class="title__firstword">${result[1]}</span>  
-                            sent you 
-                            <span class="title__number">${result[5]}</span> 
-                            IMD-coins!
-                        </h4>`;
-                    let p = `<p class="transfer__message">"${result[7]}"</p>`;
-                    let div = `<div class="transfer"> ${name + p}</div>`;
+                                    <span class="title__firstword">${result[1]}</span>  
+                                    sent you 
+                                    <span class="title__number">${result[5]}</span> 
+                                    IMD-coins!
+                                </h4>`;
+                    let message = `<p class="transfer__message">"${result[7]}"</p>`;
+                    let date = `<div class="transfer__date">
+                                    <p class="date__date">19/05/2022</p>
+                                    <p class="date__time"> - 11:13</p>
+                                </div>`;
+                    let div = `<div class="transfer"> ${name + message + date}</div>`;
 
                     document.querySelector("#tranfer__list").innerHTML += div;
                 });
