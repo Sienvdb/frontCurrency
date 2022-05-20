a304426cc625c994ec660658ee0b706480fe6b4b
MarieSerroyen <marie.serroyen@gmail.com>
2022-05-20 11:14:51 +0200

    fix: accidentally removed password id

--- classes/App.js
+++ classes/App.js
@@ -1,30 +1,3 @@
 export default class App {
-    constructor() {
-        this.getCoins();
-    }
-
-    getCoins() {
-        
-        //console.log(coins);
-
-        fetch('https://currency-backend-mms.herokuapp.com/api/v1/getCoins', {
-            method: "get",
-            headers: {
-                'Content-Type': 'application/json',
-                "Authorization": "Bearer " + localStorage.getItem('token')
-            }
-        }).then(response => {
-            return response.json();
-        }).then(json => {
-            if(json.status === "success") {
-                //console.log("👍👌👌");
-                document.querySelector("#user-coins").innerHTML = json.data.coins;
-
-                
-            } if(json.status === "error") {
-                console.log(json.message);
-                
-            }
-        })
-    }
+    
 }
--- main.js
+++ main.js
@@ -3,5 +3,8 @@ import './sass/app.scss';
 import App from "./classes/App";
 const app = new App();
 
-import Transfer from "./classes/Transfer.js";
+/*import Transfer from "./classes/Transfer.js";
 const transfer = new Transfer();
+
+import Coins from "./classes/Coins.js";
+const coins = new Coins();*/
