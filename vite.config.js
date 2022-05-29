508597f30e6e344de36b1b52c215d74fba71b7b9
MarieSerroyen <marie.serroyen@gmail.com>
2022-05-29 19:32:14 +0200

    feat: added signup.js to heroku deployment

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,8 @@ module.exports = defineConfig({
         transfer: resolve(__dirname, 'transfer.html'),
         transactions: resolve(__dirname, 'transactions.html'),
         leaderboard: resolve(__dirname, 'leaderboard.html'),
-        loginjs: resolve(__dirname, 'javascript/login.js')
+        loginjs: resolve(__dirname, 'javascript/login.js'),
+        signupjs: resolve(__dirname, 'javascript/signup.js')
       }
     }
   }
