d3023c9a5bca37d4dac9169fe55eb2393d0154bc
MarieSerroyen <marie.serroyen@gmail.com>
2022-05-29 18:51:35 +0200

    feat: added all html files to vite.config

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,11 @@ module.exports = defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        nested: resolve(__dirname, 'login.html')
+        nested: resolve(__dirname, 'login.html'),
+        nested: resolve(__dirname, 'signup.html'),
+        nested: resolve(__dirname, 'transfer.html'),
+        nested: resolve(__dirname, 'transaction.html'),
+        nested: resolve(__dirname, 'leaderboard.html')
       }
     }
   }
